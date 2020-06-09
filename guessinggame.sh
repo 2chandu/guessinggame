f007b17f3873d09f1107068f6e90b5d7d0164ec3
SushmithaVaranasi <65295281+SushmithaVaranasi@users.noreply.github.com>
2020-06-09 17:07:43 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,3 +1,4 @@
+echo "This is a guessing game"
 $guess=50
 function inputs
 {
@@ -13,6 +14,7 @@ echo "your number is high"
 else
 echo "your number is low"
 fi
+echo "Try Again"
 inputs
 done
 echo "You guessed the right number"
